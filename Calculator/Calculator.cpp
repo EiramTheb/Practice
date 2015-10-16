15047a2c4b4d7ac39d8293a6caae8debe849b26f
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2015-10-16 12:09:19 +0800

    Create Calculator.cpp
    Initial commented creation of a simple terminal calculator.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************************
+FILE: CALCULATOR
+
+AUTHOR: BETH M ESTEVEZ QUINTANA
+        ARTURO TORRES IRIZARRY
+
+DESCRIPTION: Basic calculator that runs in terminal/command line, no GUI.
+**************************************************************************************/
