f6072ab6fc814b783fc233516dc7a9d1fbecab9e
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2015-10-16 14:30:37 +0800

    Guiding comments added
    Added functions and comments so you can start filling out the blanks.

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -6,3 +6,37 @@ AUTHOR: BETH M ESTEVEZ QUINTANA
 
 DESCRIPTION: Basic calculator that runs in terminal/command line, no GUI.
 **************************************************************************************/
+#include <iostream>
+using namespace std;
+
+float add(float a, float b)
+{
+  // Implement a formula that will add a and b, and then return the result
+  
+}
+
+float substract(float a, float b)
+{
+  // Implement a formula that will substract a and b, and then return the result
+  
+}
+
+float multiply(float a, float b)
+{
+  // Implement a formula that will multiply a and b, and then return the result
+  
+}
+
+float divide(float a, float b)
+{
+  // Implement a formula that will divide a and b, and then return the result
+  
+}
+
+int main ()
+{
+  
+  // Find a way to constantly keep reading inputs and displaying results until user exits
+  
+  return 0;
+}
