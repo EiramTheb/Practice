6ef20a45aaa2c2ad8c57b1092517987e8a8aa991
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2015-10-19 14:34:06 +0800

    Create arrayManipulation.cpp
    Create arrayManipulation.cpp, with back bone. Need to fill in the algorithm.

---
+++ Array-Manipulation/arrayManipulation.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************************
+FILE: ARRAY MANIPULATION
+
+AUTHOR: EIRAMTHEB
+        ELBARTOWASHERE
+        
+DESCRIPTION: When completed, the following program should first fill in (by asking 
+the user for input) and then list all the elements in an array. For example:
+
+User Input: 3 6 17 9 4 25 0 8
+3
+6
+17
+9
+4
+25
+0
+8
+
+NOTE: If you want to make it more challenging, make it dynamic....
+
+************************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  // Create an array of the desired size.
+  
+  
+  // Store user inputs
+  
+  
+  // Output all the inputs stored in the array
+  
+
+}
