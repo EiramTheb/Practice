a674791b952ec44d39f55ca5bbb480778bfa4c49
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2015-10-19 10:22:22 +0800

    Create Guessing.cpp
    Created guessing game. Need to fill in the body.

---
+++ Guessing/guessingGame.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************************
+FILE: GUESSING
+AUTHOR: EIRAMTHEB
+        ELBARTOWASHERE
+DESCRIPTION: The following program will act as a guessing game in which the user has 
+eight tries to guess a randomly generated number. The program will tell the user each
+time whether he guessed high or low
+*************************************************************************************/
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  // Create variable that will have a random number, variable to keep count, and
+  // variable to store user's guess
+  
+  
+  // Create loop that will keep asking user to guess until he actually guesses 
+  // the correct answer or until the user runs out of tries. If the user doesn't
+  // guess correctly, let user know if he needs to go higher or lower.
+  
+  
+  // Tell the user if he guessed the right number or not
+  
+  
+}
