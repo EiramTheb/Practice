a21160929eaaa8e893556c60b8dabc9606c998a0
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2015-10-19 13:07:26 +0800

    Create permutation.cpp
    Created permutation.cpp back bone. Need to fill in with algorithm.

---
+++ Permutation/permutation.cpp
@@ -0,0 +1,31 @@
+/*************************************************************************************
+FILE: PERMUTATION
+
+AUTHOR: EIRAMTHEB
+        ELBARTOWASHERE
+
+DESCRIPTION: Write a program to display all possible permutations of a given input 
+string. If the string contains duplicate characters, you may have multiple repeated 
+results. Input should be of the form:
+
+permute string
+
+and output should be a word per line. Here is a sample for the input 'cat':
+cat
+cta
+act
+atc
+tac
+tca
+**************************************************************************************/
+#include<iostream>
+using namespace std;
+
+// Create variables
+
+int main()
+{
+
+  // Fill with algorithm here
+
+}
