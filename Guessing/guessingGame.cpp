a2830bc69549219d42d4c5b751b8c5e21a86a263
EiramTheb <bethmarie.estevez@gmail.com>
2017-01-30 09:52:28 -0600

    Update guessingGame.cpp

--- Guessing/guessingGame.cpp
+++ Guessing/guessingGame.cpp
@@ -25,27 +25,35 @@ time whether he guessed high or low
   
   // Tell the user if he guessed the right number or not
   
-  //Preliminary work= first try; doesn't quite address all challenges
+
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib>      //Adds library for random generated numbers
 using namespace std;
 
 int main () 
 {
-  int x=1;
   int y;
-  int z= 1 + (rand()% 20);      //Decided to put it out here so the value doesn't get recalculated
+        
+  //Decided to put it out here so the value doesn't get recalculated
+  //Stores a randomly generated number, value from 1 to 20, in variable z
+  int z= 1 + (rand()% 20);    
  
   cout<< "I'm thinking of a number between 1 and 20. Can you guess it? GO!"<< endl;
   cout<< "Go ahead, enter your guess, you have 8 tries."<< endl;      
   
+  //Parameters, x starts at value one and will go up one value with every loop. Once it reaches the value of 8 the 
+  //program will close.
   for(x = 1; x <= 8; x++){
-
+  
+  //Store the user's guess
   cin>> y;
   
+  //Compares user's guess (y) to the randomly generated number (z)
+  //exits the program once the user guesses right; continues to allow guesses until 8 guesses and tells user if his 
+  //guess is high or low
   if (y==z){
       cout<< "You guessed right! \n Now Bye-Bye!"<< endl;
-      return 0;}                        //exits the program once the user guesses right
+      return 0;}                       
   
     else if (y<z)
       cout<< "You went too low :( \n Why don't you try again?" << endl;
